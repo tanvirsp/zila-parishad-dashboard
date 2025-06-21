7cfd841957716b6c9be0889429a1f994630dcbf4
Mir Raisul Tanvir <tanvirgraphic@gmail.com>
2025-06-21 19:51:17 +0600

    url update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000'
-        // target: 'https://admin.zpjamalpur.com'
+        // target: 'http://localhost:5000'
+        target: 'http://zilaporishad.ajkerjamalpur.com'
       }
     }
   }
